933aff522cd4c307c5d96c7e9a469b6f2625139a
mac <mac@123123.local>
2016-06-30 01:23:59 -0700

    初始化代码

---
+++ Git2/Git2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git2
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 Yh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
